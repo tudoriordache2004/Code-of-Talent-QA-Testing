84dcfe731fb64bcc661bd7291afd047a46b11f3c
tudoriordache2604 <tudor-dimitrie.iordache@s.unibuc.ro>
2025-10-01 22:23:26 +0300

    -- Register with a code

---
+++ src/main/java/org/example/RegisterWithCode.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RegisterWithCode {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://cotcampus.codeoftalent.com/login");
+
+        WebElement registerCode = driver.findElement(By.className("have-code"));
+        registerCode.click();
+
+        String code = "grNTmJUN";
+
+        for (int i = 0; i < code.length(); i++) {
+            String fieldId = "app_user_registration_code_" + i;
+            WebElement inputBox = driver.findElement(By.id(fieldId));
+            inputBox.sendKeys(Character.toString(code.charAt(i)));
+        }
+
+        WebElement emailBox = driver.findElement(By.id("app_user_registration_email"));
+        emailBox.sendKeys("tudor@test.com");
+
+        WebElement passwordBox = driver.findElement(By.id("app_user_registration_email2"));
+        passwordBox.sendKeys("tudor@test.com");
+
+        WebElement nameBox = driver.findElement(By.id("app_user_registration_first_name"));
+        nameBox.sendKeys("testnume");
+
+        WebElement prenumeBox = driver.findElement(By.id("app_user_registration_last_name"));
+        prenumeBox.sendKeys("testprenume");
+
+        WebElement registerButton = driver.findElement(By.xpath("//button[@type='submit' and contains(text(),'Register')]"));
+        registerButton.click();
+    }
+}
