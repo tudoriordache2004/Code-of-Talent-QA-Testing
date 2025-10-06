dc0081f2bc900dee247fc7cee6533b03f25cb765
tudoriordache2604 <tudor-dimitrie.iordache@s.unibuc.ro>
2025-10-06 14:23:51 +0300

    -- Register with a code (solved)

--- src/main/java/org/example/RegisterWithCode.java
+++ src/main/java/org/example/RegisterWithCode.java
@@ -25,8 +25,8 @@ public class RegisterWithCode {
         WebElement emailBox = driver.findElement(By.id("app_user_registration_email"));
         emailBox.sendKeys("tudor@test.com");
 
-        WebElement passwordBox = driver.findElement(By.id("app_user_registration_email2"));
-        passwordBox.sendKeys("tudor@test.com");
+        WebElement repeatEmailBox = driver.findElement(By.id("app_user_registration_email2"));
+        repeatEmailBox.sendKeys("tudor@test.com");
 
         WebElement nameBox = driver.findElement(By.id("app_user_registration_first_name"));
         nameBox.sendKeys("testnume");
@@ -36,5 +36,15 @@ public class RegisterWithCode {
 
         WebElement registerButton = driver.findElement(By.xpath("//button[@type='submit' and contains(text(),'Register')]"));
         registerButton.click();
+
+        WebElement password = driver.findElement(By.id("app_user_set_password_by_user_plainPassword"));
+        password.sendKeys("TudorQACOT1!");
+
+        WebElement repeatPassword = driver.findElement(By.id("app_user_set_password_by_user_plainPassword2"));
+        repeatPassword.sendKeys("TudorQACOT1!");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button.btn.btn-primary.btn-block[type='submit']"));
+        submitButton.click();
+
     }
 }
